ea43e3fd6ef57b5b9bb3d2c910dadcc54c18c791
ibukiyabu <ibuki.y2115@gmail.com>
2024-03-02 21:30:47 +0900

    idの型間違ってた

--- src/com/diworksdev/ecsite/dto/BuyItemDTO.java
+++ src/com/diworksdev/ecsite/dto/BuyItemDTO.java
@@ -2,14 +2,14 @@ package com.diworksdev.ecsite.dto;
 
 public class BuyItemDTO {
 	
-	public String id;
+	public int id;
 	public String itemName;
 	public String itemPrice;
 	
-	public String getId() {
+	public int getId() {
 		return id;
 	}
-	public void setId(String id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 	public String getItemName() {
