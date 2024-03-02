f396b40c9c36e76cfdbf8b83b763bd9ae9382d4f
ibukiyabu <ibuki.y2115@gmail.com>
2024-03-02 21:20:43 +0900

    一部修正

--- src/com/diworksdev/ecsite/action/HomeAction.java
+++ src/com/diworksdev/ecsite/action/HomeAction.java
@@ -4,9 +4,12 @@ import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
+import com.diworksdev.ecsite.dao.BuyItemDAO;
+import com.diworksdev.ecsite.dto.BuyItemDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class HomeAction extends ActionSupport implements SessionAware{
+	public Map<String, Object>session;
 
 	public String execute() {
 		String result = "login";
@@ -25,12 +28,11 @@ public class HomeAction extends ActionSupport implements SessionAware{
 	}
 	
 	@Override
-	
-	public void setSession(Map<String, Object>sesiion) {
+	public void setSession(Map<String, Object>session) {
 		this.session = session;
 	}
 	
-	public Map<String, Object> getSession(){
-		return this.session();
+	public Map<String, Object> getSession() {
+		return this.session;
 	}
 }
