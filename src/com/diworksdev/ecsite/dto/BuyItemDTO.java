97bbadd95edf3d23717db60208920b1cba878d21
ibukiyabu <ibuki.y2115@gmail.com>
2024-03-02 17:33:27 +0900

    BuyItemDTO.java作成〜記述

---
+++ src/com/diworksdev/ecsite/dto/BuyItemDTO.java
@@ -0,0 +1,28 @@
+package com.diworksdev.ecsite.dto;
+
+public class BuyItemDTO {
+	
+	public String id;
+	public String itemName;
+	public String itemPrice;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public String getItemPrice() {
+		return itemPrice;
+	}
+	public void setItemPrice(String itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+
+}
